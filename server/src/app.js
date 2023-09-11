24bffe2ebc93840a21faecef84fffe3f0cfeec8f
Sidhil Sivadas Manoli (Trianz) <sidhilsivadas.m@trianz.com>
2023-09-11 12:45:04 +0530

    server init

--- server/src/app.js
+++ server/src/app.js
@@ -7,11 +7,6 @@ const launchRouter = require("./routes/launches/launches.routes");
 const app = express();
 app.use(cors());
 app.use(morgan('combined'));
-app.use(
-    bodyParser.urlencoded({
-      extended: true,
-    }),
-);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use("/planets",planetRouter);
